a30161228fc8e2bb5a378ad049417b66acde2df6
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-11 01:35:15 +0300

    fixed bpttom neck

--- src/components/ImageComponents.jsx
+++ src/components/ImageComponents.jsx
@@ -3,12 +3,19 @@ import styles from "./../styles/ImageComponent.module.scss";
 
 function ImageComponent() {
   return (
-    <img
-      loading="lazy"
-      src="https://cdn.builder.io/api/v1/image/assets/TEMP/3b2853b880b35c3cc66083685d9403279c787cafdbf5f893216a7d28dc6b6abb?apiKey=69c943fd599c485fb32c02233b347491&"
-      className={styles.image}
-      alt="Description of the image"
-    />
+    <div className={styles.container}>
+      <img
+        loading="lazy"
+        src="/images/background2.png"
+        className={styles.image}
+        alt="Description of the image"
+      />
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Do you have other questions? <br/>
+        Feel free to reach out to us.</h1>
+        <button className={styles.button}><p className={styles.buttonText}>Contact Us</p></button>
+      </div>
+    </div>
   );
 }
 
