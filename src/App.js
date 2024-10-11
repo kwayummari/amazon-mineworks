9823180aed55dbc7a9d4277c8853010718e4ddd0
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-12 02:13:18 +0300

    fixed bpttom neck

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import styles from './styles/LandingPage.module.scss';
 import ImageComponent from './components/ImageComponents';
 import LandingPage from './pages/landing-page';
 import AtAGlance from './pages/at-a-glance';
+import SubHeader from './components/SubHeader';
+
+const ConditionalHeader = () => {
+  const location = useLocation();
+  return location.pathname === '/' ? <Header /> : <SubHeader />;
+};
 
 const App = () => (
   <Router>
     <div className={styles.landingPage}>
-      <Header />
+      <ConditionalHeader />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/at-a-glance" element={<AtAGlance />} />
--- src/pages/at-a-glance.js
+++ src/pages/at-a-glance.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
-import About from '../components/About';
-import Services from '../components/Service';
-import Gallery from '../components/Gallery';
-import News from '../components/News';
-import FAQ from '../components/FAQ';
-import Footer from '../components/Footer';
 import styles from '../styles/LandingPage.module.scss';
-import ImageComponent from '../components/ImageComponents';
 
 const AtAGlance = () => (
   <div className={styles.landingPage}>
-    <Header />
-    <About />
-    <Services />
-    <Gallery />
-    <News />
-    <FAQ />
-    <ImageComponent />
-    <Footer />
+    
   </div>
 );
 
