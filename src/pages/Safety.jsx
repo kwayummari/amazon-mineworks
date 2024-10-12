b12c28017a9b301270b52fed1c67526b8fef479d
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-12 06:36:57 +0300

    fixed bpttom neck

--- src/pages/Safety.jsx
+++ src/pages/Safety.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from '../styles/LandingPage.module.scss';
 import SafetyDocumentation from '../components/SafetyDocumentation';
 import SafetyMilestone from '../components/SafetyMilestone';
+import SafetyData from '../components/SafetyData';
 
 const Safety = () => (
   <div className={styles.landingPage}>
     <SafetyDocumentation />
     <SafetyMilestone />
+    <SafetyData />
   </div>
 );
 
